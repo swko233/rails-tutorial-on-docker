70ffd9cfa2adbda4bff2dda73f463113e0a30d4d
swko233 <swkon233@gmail.com>
2018-10-22 23:12:26 +0900

    ページ遷移時のflashメッセージ削除の設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # binding pry
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+      # redirect_to help_path
+    end
+  end
+  
+  def destroy
+  end
+
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe 'POST /login' do
+    it 'render new with invalid params' do
+      post :create, params: {session: {email: "", password: ""}}
+      expect(response).to render_template(:new)
+    end
+    
+    it 'create flash of danger with invalid params' do
+      post :create, params: {session: {email: "", password: ""}}
+      expect(flash[:danger]).not_to be_blank
+    end
+
+    it 'delete flash-message by moving other pages' do
+      post :create, params: {session: {email: "", password: ""}}
+      get :new
+      expect(flash.now[:danger]).to be_blank
+    end
+
+  end
+
 end
